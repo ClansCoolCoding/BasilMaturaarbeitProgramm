bce2b35bdb7d736615c5b9a022933f5397a6c4d5
ClansCoolCoding <fischer.basil@gmail.com>
2022-07-07 15:29:37 +0200

    DES prinzipiell Fertig Square And Multiply Fertig RSA Fertig Zusammenfügung RSA/DES Fertig Simple DES Implementation Ausstehend

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         int mtd = 6;
         Message msg = new Message("Hello There!", "00010100110000101110110001011100110011110010000000101100110010000110010101100110011000100110000100000110");
         Code coder = new Code(msg);
-        EncryptionStatus e = EncryptionStatus.DECRYPT;
+        EncryptionStatus status = EncryptionStatus.DECRYPT;
 
         Person[] personen = new Person[5];
         personen[0] = new Person("Person 1", 7, 19, 21);
@@ -16,11 +16,10 @@ public class Main {
         personen[3] = new Person("Person 4", 5, 11, 14);
         personen[4] = new Person("Person 5", 3, 7, 10);
 
-        switch(e){
+        switch(status){
             case ENCRYPT: System.out.println(coder.encode(personen[0], mtd)); break;
             case DECRYPT: System.out.println(coder.decode(personen[0])); break;
         }
-
 
     }
 }
